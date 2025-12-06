7e8651190e6a987853895aaee15af3f78f12bfd5
moon-cse4302 <fatema@iut-dhaka.edu>
2025-12-06 19:14:08 +0600

    Update config, store and app icon

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,7 +7,7 @@ import { Alert } from "react-native";
 import { Content, GoogleGenerativeAI } from "@google/generative-ai";
 import { GEMINI_API_KEY } from "@/Keys";
 import { UserData, Activity, ExerciseData, Meal, MealData, ChatMessage } from "@/global";
-import RNFS from 'react-native-fs';
+import * as RNFS from 'react-native-fs';
 
 let subscription: Pedometer.Subscription;
 
